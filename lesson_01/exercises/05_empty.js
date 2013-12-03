926dd44a4e3b623fb3cebd26d95ffd4de07e5bbe
Greg Cerveny <greg.cerveny@gmail.com>
2013-12-03 13:00:44 -0600

    new empty exercise

---
+++ lesson_01/exercises/05_empty.js
@@ -0,0 +1,11 @@
+// Given a string, display whether it is empty or not
+// Hint: A string length can be determined with testString.length
+
+var testString = "I have content!"
+// empty testString example:
+// var testString = ""
+
+// your code goes here
+  console.log("Is empty")
+// your code goes here
+  console.log("Is not empty")
